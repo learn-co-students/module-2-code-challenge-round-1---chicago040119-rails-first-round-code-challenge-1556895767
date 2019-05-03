2a48228336c48afb43950e9037dbb5f5fd4eb6ed
Jose Elera <jelera@gmail.com>
2019-05-03 11:11:33 -0500

    Main challenge DONE!!

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,37 @@
 class HeroinesController < ApplicationController
+  before_action :set_heroine, only: [:show]
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @power = @heroine.power
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      @errors = @heroine.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def set_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
