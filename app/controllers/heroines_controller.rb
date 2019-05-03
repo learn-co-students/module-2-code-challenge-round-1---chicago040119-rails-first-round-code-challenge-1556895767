502ca76ed2dee423ceddeb2ce30c696c3f4e7910
Jackson Robbins <jackrobbins1@gmail.com>
2019-05-03 10:51:47 -0500

    finished some work on the heroines create

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,19 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.new
+
+
+  end
 end
